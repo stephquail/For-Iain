50cfa68001c3b6a0cfb654e82411adc91c96e508
Steph Quail <stephanie.quail@gmail.com>
2015-10-27 14:21:20 +1100

    More commenting detail

--- analysis.R
+++ analysis.R
@@ -1,7 +1,36 @@
 ###This script runs the data extraction and summary for all stages of the PIT & Devaluation task
 
+## This runs the data extraction scripts for the instrumental, transfer and devaluation parts of the PIT and Deval task. 
+## Extraction of PAVLOVIAN TRAINING to come.
 
-##Before you run this have you:
+## If this is the first time you are running this, check that the appropriate packages have all been installed.
+## Uncomment this if you need to install these packages
+
+##install.packages("ggplot2") # installs ggplot for graphing
+##install.packages("reshape2")
+##install.packages("stringr")
+
+## Installing packages through the install.packages function might not work depending on Uni firewall stuff. So may take a bit of fiddling/downloading the packages directly. 
+
+## Structure of this project (make sure these folders exist before running this script)
+##  analysis.R
+##  instru_analysis.R
+##  pav_analysis.R
+##  transfer_analysis.R
+##  deval_analysis.R
+##
+##  /R
+##    functions.R #sets up participant data IDs, and functions to be used by analysis scripts
+##    /data # this is read only. no changes are made to data in these folders. Keep the raw data raw!
+##      /instru
+##      /pav
+##      /transfer
+##      /deval
+##    /output
+##      /data # all extracted group data written to here
+##      /figures
+
+## Before you run this have you:
 ##  - Put all appropriate raw data files into the R/data folder?
 ##  - Are all raw data files named appropriately?
 ##  - Have you given all participants the appropriate ID vectors in the 'R/functions.R' script?
